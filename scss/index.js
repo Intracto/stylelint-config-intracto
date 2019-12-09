dccc5ac635bae0ef6746d2a00a63d53dab3f5deb
yulian.alexeyev <yulian.alexeyev@intracto.com>
2019-12-09 14:07:29 +0100

    added some linting rules

--- scss/index.js
+++ scss/index.js
@@ -4,5 +4,19 @@ module.exports = {
     "stylelint-prettier/recommended"
   ],
   plugins: ["stylelint-scss"],
-  rules: {}
+  rules: {
+    "color-named": "never",
+    "comment-empty-line-before": [
+      "always",
+      {
+        except: ["first-nested"],
+        ignore: ["after-comment", "stylelint-commands"]
+      }
+    ],
+    "comment-whitespace-inside": "always",
+    "function-url-quotes": "always",
+    "no-unknown-animations": true,
+    "selector-pseudo-element-colon-notation": "double",
+    "shorthand-property-no-redundant-values": true
+  }
 };
