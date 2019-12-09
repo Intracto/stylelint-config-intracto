609c5cc858e2af5e3756a63d261335cbeaf757a4
yulian.alexeyev <yulian.alexeyev@intracto.com>
2019-12-09 11:35:10 +0100

    remove airbnb config, repo is not maintained

--- scss/index.js
+++ scss/index.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: [
     "stylelint-config-recommended-scss",
-    "stylelint-config-airbnb",
     "stylelint-prettier/recommended"
   ],
   plugins: ["stylelint-scss"],
